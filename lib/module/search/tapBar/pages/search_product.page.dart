1bda58aeaefe57fe0d69d6c822ee6ac7cc74e31c
abdu ahmed <a01226967080@gmail.com>
2022-02-03 17:40:28 +0200

    create search by product and category

--- lib/module/search/tapBar/pages/search_product.page.dart
+++ lib/module/search/tapBar/pages/search_product.page.dart
@@ -1,24 +1,98 @@
+import 'package:firestore_model/firestore_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:salla_app/data/models/product.dart';
+import 'package:salla_app/helper/app.theme.dart';
 import 'package:salla_app/helper/app.widget.dart';
 
-class SearchProduct extends StatelessWidget {
-  const SearchProduct({Key key}) : super(key: key);
+import '../../../../helper/app.routes.dart';
+
+class SearchProduct extends StatefulWidget {
+  @override
+  State<SearchProduct> createState() => _SearchProductState();
+}
+
+class _SearchProductState extends State<SearchProduct> {
+  TextEditingController _textEditingController = TextEditingController();
+  String searchValue = '';
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            SizedBox(height: 20.h),
-            CustomTextForm(
-              hintText: 'البحث عن منتج',
-            )
-          ],
+    return ModelStreamGetBuilder<Product>(
+        // query: (q) =>
+        //     q.where('description', isGreaterThanOrEqualTo: searchValue),
+        builder: (
+      context,
+      snapshot,
+    ) {
+      return Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CustomTextForm(
+                controller: _textEditingController,
+                onChanged: (value) {
+                  setState(() {
+                    searchValue = value;
+                  });
+                },
+                hintText: 'Search',
+              ),
+              SizedBox(
+                height: 8.h,
+              ),
+              searchValue.length >= 1
+                  ? Expanded(
+                      child: ListView.builder(
+                          itemCount: snapshot.data.length,
+                          itemBuilder: (context, index) {
+                            String titleList = snapshot.data[index].title;
+                            String descriptionList =
+                                snapshot.data[index].description;
+                            Product product = snapshot.data[index];
+                            if (titleList.contains(searchValue) ||
+                                descriptionList.contains(searchValue)) {
+                              return InkWell(
+                                onTap: () {
+                                  Modular.to.pushNamed(AppRoutes.product,
+                                      arguments: product);
+                                },
+                                child: Card(
+                                    elevation: 4,
+                                    // margin: EdgeInsets.all(20),
+                                    color: Colors.grey[200],
+                                    child: ListTile(
+                                      trailing: Text(
+                                        ' ${product.price}  جنية',
+                                        style: GoogleFonts.cairo(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 18.sp,
+                                            color: AppTheme.primaryColor),
+                                      ),
+                                      title: Text(
+                                        product.title,
+                                        style: GoogleFonts.cairo(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 18.sp,
+                                            color: AppTheme.primaryColor),
+                                      ),
+                                      leading: CircleAvatar(),
+                                    )),
+                              );
+                            }
+                            return SizedBox();
+                          }),
+                    )
+                  : SizedBox(),
+            ],
+          ),
         ),
-      ),
-    );
+      );
+    });
   }
 }
--- lib/module/search/tapBar/pages/search_type.page.dart
+++ lib/module/search/tapBar/pages/search_type.page.dart
@@ -1,24 +1,81 @@
+import 'package:firestore_model/firestore_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:salla_app/data/models/category.dart';
+import 'package:salla_app/helper/app.routes.dart';
 import 'package:salla_app/helper/app.widget.dart';
 
-class SearchType extends StatelessWidget {
-  const SearchType({Key key}) : super(key: key);
+class SearchType extends StatefulWidget {
+  @override
+  State<SearchType> createState() => _SearchTypeState();
+}
 
+class _SearchTypeState extends State<SearchType> {
+  TextEditingController _textEditingController = TextEditingController();
+  String searchValue = '';
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            SizedBox(height: 20.h),
-            CustomTextForm(
-              hintText: 'البحث عن نوع',
-            )
-          ],
+    return ModelStreamGetBuilder<Category>(
+        // query: (q) =>
+        //     q.where('description', isGreaterThanOrEqualTo: searchValue),
+        builder: (
+      context,
+      snapshot,
+    ) {
+      return Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CustomTextForm(
+                controller: _textEditingController,
+                onChanged: (value) {
+                  setState(() {
+                    searchValue = value;
+                    print(searchValue.length);
+                    print(searchValue);
+                  });
+                },
+                hintText: 'Search',
+              ),
+              SizedBox(
+                height: 8.h,
+              ),
+              searchValue.length >= 1
+                  ? Expanded(
+                      child: ListView.builder(
+                          itemCount: snapshot.data.length,
+                          itemBuilder: (context, index) {
+                            String titleList = snapshot.data[index].title;
+
+                            Category category = snapshot.data[index];
+                            if (titleList.contains(searchValue)) {
+                              return InkWell(
+                                onTap: () {
+                                  Modular.to.pushNamed(AppRoutes.category,
+                                      arguments: category);
+                                },
+                                child: Card(
+                                    elevation: 4,
+                                    // margin: EdgeInsets.all(20),
+                                    color: Colors.grey[200],
+                                    child: ListTile(
+                                      title: Text(category.title),
+                                      leading: CircleAvatar(),
+                                    )),
+                              );
+                            }
+                            return SizedBox();
+                          }),
+                    )
+                  : SizedBox(),
+            ],
+          ),
         ),
-      ),
-    );
+      );
+    });
   }
 }
