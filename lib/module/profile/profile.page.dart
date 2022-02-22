46207d73daf7eb0fd8845189c55b314fc8c4a374
abdu ahmed <a01226967080@gmail.com>
2022-02-23 01:02:47 +0200

    edit profile.page.dart

--- lib/module/profile/profile.page.dart
+++ lib/module/profile/profile.page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:salla_app/helper/app.routes.dart';
 import 'package:salla_app/helper/app.widget.dart';
 import 'package:salla_app/helper/notifications.dart';
@@ -12,40 +13,63 @@ class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Column(
-        children: [
-          CustomListTile(
-            text: 'المعلومات الشخصية',
-            onTap: () {
-              AuthService.isLogin
-                  ? Modular.to.pushNamed(AppRoutes.personal)
-                  : Notifications.error('يجب تسجيل الدخول');
-            },
-            icon: Icons.person,
-          ),
-          CustomListTile(
-            text: 'عنوانك',
-            onTap: () {},
-            icon: Icons.location_on,
-          ),
-          CustomListTile(
-            text: 'طرق الدفع',
-            onTap: () {},
-            icon: Icons.card_travel,
-          ),
-          CustomListTile(
-            text: 'تسجيل الخروج',
-            onTap: () {
-              Modular.get<AppBloc>().logOut();
-              Modular.to.pushNamed(AppRoutes.mainHome);
-              Modular.get<AppBloc>().changeIndexTo(0);
-            },
-            icon: Icons.logout,
-          ),
-        ],
-      ),
-    ));
+        body: AuthService.isLogin
+            ? Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    CustomListTile(
+                      text: 'المعلومات الشخصية',
+                      onTap: () {
+                        AuthService.isLogin
+                            ? Modular.to.pushNamed(AppRoutes.personal)
+                            : Notifications.error('يجب تسجيل الدخول');
+                      },
+                      icon: Icons.person,
+                    ),
+                    CustomListTile(
+                      text: 'عنوانك',
+                      onTap: () {},
+                      icon: Icons.location_on,
+                    ),
+                    CustomListTile(
+                      text: 'طرق الدفع',
+                      onTap: () {},
+                      icon: Icons.card_travel,
+                    ),
+                    CustomListTile(
+                      text: 'تسجيل الخروج',
+                      onTap: () {
+                        Modular.get<AppBloc>().logOut();
+                        // Modular.to.pushNamed(AppRoutes.mainHome);
+                        Modular.get<AppBloc>().changeIndexTo(0);
+                      },
+                      icon: Icons.logout,
+                    ),
+                  ],
+                ),
+              )
+            : Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'يجب تسجيل الدخول اولا',
+                      style: GoogleFonts.cairo(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    GradientButton(
+                      text: 'تسجيل الدخول',
+                      textColor: Colors.white,
+                      onPressed: () {
+                        Modular.to.pushNamed(AppRoutes.login);
+                      },
+                    ),
+                  ],
+                ),
+              ));
   }
 }
